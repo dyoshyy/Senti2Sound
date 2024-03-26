caf23fe736e4e438b88e42035e15d007300b373b
dyoshyy <d.yoshhyy@gmail.com>
2024-03-26 01:14:36 +0000

    fix: nltkの修正

--- src/my_modules/sentiment_analyzer.py
+++ src/my_modules/sentiment_analyzer.py
@@ -3,10 +3,7 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.corpus.util import LazyCorpusLoader
 
 nltk.data.path.append('/opt/nltk_data')
-
-# vader_lexiconが存在しない場合のみダウンロード
-if 'vader_lexicon' not in LazyCorpusLoader('vader_lexicon', nltk.corpus.reader.VaderCorpusReader, ['vader_lexicon.zip']).__dict__:
-    nltk.download('vader_lexicon')
+nltk.download('vader_lexicon')
 
 # 単語の感情分析
 def analyze_sentiment(word):
